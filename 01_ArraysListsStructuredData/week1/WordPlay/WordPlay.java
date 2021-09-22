9093f3ba71e7477fd37e800c995c88c64add546a
Joe Thor <joethor1989@gmail.com>
2021-09-22 17:28:41 -0500

    assignment one

---
+++ 01_ArraysListsStructuredData/week1/WordPlay/WordPlay.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of WordPlay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WordPlay {
+    
+/* Write a method isVowel that has one Char parameter named ch. This method returns true 
+if ch is a vowel (one of 'a', 'e', 'i', 'o', or 'u' or the uppercase versions) and false otherwise. 
+You should write a tester method to see if this method works correctly. For example, 
+isVowel(‘F’) should return false, and isVowel(‘a’) should return true.  */
+    public boolean isVowel(char ch){
+    
+    return true;
+    }
+ 
+/*
+ * Write a method replaceVowels that has two parameters, a String named phrase and a 
+ * Char named ch. This method should return a String that is the string phrase with all
+ * the vowels (uppercase or lowercase) replaced by ch. For example, the call replaceVowels(“Hello World”, ‘*’) 
+ * returns the string “H*ll* W*rld”. Be sure to call the method isVowel that you wrote and also test this method.
+ */   
+
+/*
+ *     Write a method emphasize with two parameters, a String named phrase and a character named ch. This method should return a String 
+ *     that is the string phrase but with the Char ch (upper- or lowercase) replaced by
+
+     ‘*’ if it is in an odd number location in the string (e.g. the first character has an odd number location but an even index, it is at index 0), or
+
+     ‘+’ if it is in an even number location in the string (e.g. the second character has an even number location but an odd index, it is at index 1).
+     
+     
+ */
+}
