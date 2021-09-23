b3ff71bae8c387284ed608b8846e0cdb7557966d
Joe Thor <joethor1989@gmail.com>
2021-09-22 20:45:51 -0500

    wordplay is done

--- 01_ArraysListsStructuredData/week1/WordPlay/WordPlay.java
+++ 01_ArraysListsStructuredData/week1/WordPlay/WordPlay.java
@@ -12,16 +12,72 @@ if ch is a vowel (one of 'a', 'e', 'i', 'o', or 'u' or the uppercase versions) a
 You should write a tester method to see if this method works correctly. For example, 
 isVowel(‘F’) should return false, and isVowel(‘a’) should return true.  */
     public boolean isVowel(char ch){
+        
+        String vowelString = "aeiou";
+        
+        char lowerCh = Character.toLowerCase(ch);
+        
+        int indexCh = vowelString.indexOf(lowerCh);
+        
+        if(indexCh != -1){
+        
+        return true;
+        }
+        else{
+        
+        return false;
+        }
+       
     
-    return true;
     }
- 
+    public void testIsVowel(){
+    
+    boolean test1 = isVowel('F');
+    
+    System.out.println( "result for test of 'F': " + test1 );  
+    
+    boolean test2 = isVowel('e');
+    
+    System.out.println( "result for test of 'e': " + test2 );  
+    
+    boolean test3 = isVowel('I');
+    System.out.println( "result for test of 'I': " + test3 );  
+
+    }
 /*
  * Write a method replaceVowels that has two parameters, a String named phrase and a 
  * Char named ch. This method should return a String that is the string phrase with all
  * the vowels (uppercase or lowercase) replaced by ch. For example, the call replaceVowels(“Hello World”, ‘*’) 
  * returns the string “H*ll* W*rld”. Be sure to call the method isVowel that you wrote and also test this method.
  */   
+
+
+public String replaceVowels(String phrase, char ch){
+    
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < phrase.length(); i++) {
+
+                char phraseChar = phrase.charAt(i);
+                
+                if(isVowel(phraseChar)){
+                    sb.insert(i, ch);
+                }
+                else{
+                    sb.insert(i, phraseChar);
+             
+                }
+        
+    }
+    System.out.println(sb);
+    return sb.toString();
+}
+
+ public void testReplaceVowels(){
+     String test = replaceVowels("Howdy Duty Boy-o!", 'J');
+     
+     String test2 = replaceVowels("You're part of chain of command that stretches all the way back to London!", 'v');
+    }
 
 /*
  *     Write a method emphasize with two parameters, a String named phrase and a character named ch. This method should return a String 
@@ -33,4 +89,38 @@ isVowel(‘F’) should return false, and isVowel(‘a’) should return true.  
      
      
  */
+
+public String emphasize(String phrase, char ch){
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < phrase.length(); i++) {
+
+                char phraseChar = phrase.charAt(i);
+                
+                if(phraseChar == ch && i%2 == 0){
+                    sb.insert(i, '+');
+                }
+                else if(phraseChar == ch  && i%2 != 0) {
+                
+                    sb.insert(i, '*');
+                }
+                else{
+                    sb.insert(i, phraseChar);
+             
+                }
+        
+    }
+    System.out.println(sb);
+    return sb.toString();
+
+
+}
+
+public void testemphasize(){
+
+    String test1 =  emphasize("Mary Bella Abracadabra", 'a') ;
+    String test2 = emphasize("dna ctgaaactga", 'a');
+
+}
 }
